78277b0f2baa9310fd245b2f297270a4d12ea64b
Francesca La Manna <57345846+LamFra@users.noreply.github.com>
2022-12-22 14:46:05 +0100

    user-story 1: function and tests

--- SmartHome.py
+++ SmartHome.py
@@ -57,7 +57,10 @@ class SmartHome:
         Checks whether the infrared distance sensor on the ceiling detects something in front of it.
         :return: True if the infrared sensor detects something, False otherwise.
         """
-        pass
+        value = GPIO.input(self.INFRARED_PIN)
+        if value != 0:
+            return False
+        return True
 
     def manage_light_level(self) -> None:
         """
--- test/SmartHomeTest.py
+++ test/SmartHomeTest.py
@@ -11,4 +11,18 @@ class SmartHomeTest(unittest.TestCase):
     """
     Your test cases go here
     """
-    pass
+
+    def setUp(self) -> None:
+        self.smart_home = SmartHome()
+
+    @patch.object(GPIO, 'input')
+    def test_check_room_occupancy_true(self, mock_occupancy):
+        mock_occupancy.return_value = 0
+        occupancy = self.smart_home.check_room_occupancy()
+        self.assertTrue(occupancy)
+
+    @patch.object(GPIO, 'input')
+    def test_check_room_occupancy_false(self, mock_occupancy):
+        mock_occupancy.return_value = 5
+        occupancy = self.smart_home.check_room_occupancy()
+        self.assertFalse(occupancy)
